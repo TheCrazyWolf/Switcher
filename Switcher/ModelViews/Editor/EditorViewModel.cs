3821214e0ad10b65722fc9eef71c9a6800f50304
TheCrazyWolf <thecrazywolf63@gmail.com>
2025-01-11 12:44:03 +0400

    Отображение названия адаптера

--- Switcher/ModelViews/Editor/EditorViewModel.cs
+++ Switcher/ModelViews/Editor/EditorViewModel.cs
@@ -1,18 +1,18 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Switcher.Managers.Adapters;
 using Switcher.Managers.Config;
 using Switcher.Models.Configs;
 
 namespace Switcher.ModelViews.Editor;
 
-public partial class EditorViewModel(ConfigurationManager configurationManager) : ObservableObject
+public partial class EditorViewModel(ConfigurationManager configurationManager, AdaptersManager adaptersManager) : ObservableObject
 {
     [ObservableProperty] private AdapterConfiguration _adapterConfiguration = new ();
-    
-    [RelayCommand]
-    public void RefreshAdapterConfigurationsCommand()
-    {
-    }
+    [ObservableProperty] private ObservableCollection<NetworkInterface> _adapters = new (adaptersManager.GetAdapters());
+    [ObservableProperty] private NetworkInterface? _selectedAdapter;
     
     [RelayCommand]
     public void Apply(AdapterConfiguration adapterConfiguration)
